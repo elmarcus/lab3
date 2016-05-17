99b2302dcb4e01ed79f8b751f4fe7fd006c09d23
Marcus <marcus.blatter@gmail.com>
2016-05-17 14:23:10 -0600

    Updated Board Functions

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -6,15 +6,23 @@ import computerPlayer.*;
 public class Controller {
 	
 	
-	public static void main()
+	public static void main(String[] args)
 	{
-		//NaivePlayer player1 = new NaivePlayer();
+		int boardSize = Integer.parseInt(args[0]);
+		
+		if(boardSize % 2 == 1)
+			boardSize++;
+			
+		Board board = new Board(boardSize);
+		
+		SmartPlayer player1 = new SmartPlayer();
 		SmartPlayer player2 = new SmartPlayer();
 		
-		Board board = new Board();
-		
 		//setup phase
-		
+		board.placePiece(boardSize/2 , boardSize/2, 1);
+		board.placePiece(boardSize/2+1 , boardSize/2+1, 1);
+		board.placePiece(boardSize/2+1 , boardSize/2, 2);
+		board.placePiece(boardSize/2 , boardSize/2+1, 2);
 		
 		//play game phase
 	}
