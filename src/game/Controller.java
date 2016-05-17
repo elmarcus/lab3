897f9b3975ba502ef8648dac0f21c180b616ba39
Marcus <marcus.blatter@gmail.com>
2016-05-17 15:44:35 -0600

    game playing mostly works

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -7,70 +7,85 @@ import board.Board;
 import computerPlayer.*;
 
 public class Controller {
-	
-	
+
+
 	public static void main(String[] args)
 	{
-		int boardSize = Integer.parseInt(args[0]);
-		boolean human = false;
-		AlphaBetaPrunner abp = new AlphaBetaPrunner(-1, boardSize, boardSize);
-				
-		Board board = new Board(boardSize);
-		
-		boardSize&=0xFE;
-		
-		System.out.println("Human Player 1? Y/N");
-		
-		Scanner s = new Scanner(System.in);
-		String userInput = s.next();
 
-		
-		if(userInput.toLowerCase().equals("y"))
+		boolean human = false;
+		AlphaBetaPrunner abp = null;
+		int boardSize = 0;
+		Board board = null;
+
+
+		Scanner s = new Scanner(System.in);
+		while(true)
 		{
-			human = true;
+			System.out.println("Select board size");
+			boardSize = s.nextInt();
+			boardSize&=0xFE;
+			board = new Board(boardSize);
+			abp = new AlphaBetaPrunner(-1, boardSize, boardSize);
+
+			System.out.println("Human Player 1? Y/N");
+
+			String userInput = s.next();
+
+			if(userInput.toLowerCase().equals("y"))
+			{
+				human = true;
+				break;
+			}
+			else
+			{
+				SmartPlayer player1 = new SmartPlayer();
+				break;
+			}
 		}
-		else
-		{
-			SmartPlayer player1 = new SmartPlayer();
-		}
-		
+
 		SmartPlayer player2 = new SmartPlayer();
-		
+
 		//setup phase
 		try
 		{
-		board.placePiece(boardSize/2 , boardSize/2, 1);
-		board.placePiece(boardSize/2+1 , boardSize/2+1, 1);
-		board.placePiece(boardSize/2+1 , boardSize/2, 2);
-		board.placePiece(boardSize/2 , boardSize/2+1, 2);
+			board.placePiece(boardSize/2 , boardSize/2, 1);
+			board.placePiece(boardSize/2+1 , boardSize/2+1, 1);
+			board.placePiece(boardSize/2+1 , boardSize/2, 2);
+			board.placePiece(boardSize/2 , boardSize/2+1, 2);
 		}
 		catch(Exception e)
 		{
 			System.err.println("I have no idea how this happened");
 		}
-		
-		
+
 		int round = 0;
-		boolean pass = false;
+		int pass = 0;
+		System.out.println(boardSize);
 		
 		//play game phase
-		while(pass)
+		while(pass < 2)
 		{
+			pass = 0;
 			round++;
 			System.out.println("ROUND: " + Integer.toString(round));
 			board.print();
-			
+
 			//PLAYER 1 TURN
 			if(!human) 
 			{
 				Option option = abp.getNextMove(board, 1);
 				//do stuff with alpha beta pruning
 				//make move
-				try {
-					board.placePiece(option.i, option.j, 1);
-				} catch (Exception e) {
-					System.err.println("MOVE EXCEPTION.PLAYER 1");
+				if(option != null)
+				{
+					try {
+						board.placePiece(option.i, option.j, 1);
+					} catch (Exception e) {
+						System.err.println("MOVE EXCEPTION.PLAYER 1");
+					}
 				}
+				else
+					pass++;
 			}
 			else
 			{
@@ -79,7 +94,7 @@ public class Controller {
 					System.out.println("Choose a row and column");
 					int row = s.nextInt();
 					int col = s.nextInt();
-					
+
 					try
 					{
 						board.placePiece(row, col, 1); 
@@ -88,20 +103,26 @@ public class Controller {
 					{
 						continue;
 					}
-					
+
 					break;
 				}
 			}
-			
+
 			//PLAYER 2 TURN
 			Option option = abp.getNextMove(board, 2);
-			
-			try {
-				board.placePiece(option.i, option.j, 2);
-			} catch (Exception e) {
-				System.err.println("MOVE EXCEPTION.PLAYER 2");
+
+			if(option != null)
+			{
+				try {
+					board.placePiece(option.i, option.j, 2);
+				} catch (Exception e) {
+					System.err.println("MOVE EXCEPTION.PLAYER 2");
+				}
 			}
+			else pass++;
 		}
 		
+		board.findScore();
+
 	}
 }
